6addbc22ae6b7bc26dc18686e626f228346a1c76
vicky seth <vickyseth9933@gmail.com>
2022-11-23 08:57:17 +0530

    update

---
+++ Terraform Variable Example/Terraform example -1/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web" {
+  ami           = "ami-090fa75af13c156b4"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  tags = {
+    Name = "userdata-demo"
+    ENV  = "Prod"
+  }
+}
+
+
+
+
+
+
