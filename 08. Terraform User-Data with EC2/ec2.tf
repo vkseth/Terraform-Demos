a2d636575145145443e13d2cd72d695560479034
vicky seth <vickyseth9933@gmail.com>
2022-12-02 13:48:18 +0530

    Terraform EC2 UserData

--- 08. Terraform User-Data with EC2/ec2.tf
+++ 08. Terraform User-Data with EC2/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "my-ec2-vm" {
-  ami                    = var.ec2_ami_id
-  instance_type          = "t2.micro"
-  key_name               = "centos-eks"
-  count                  = var.ec2_instance_count
+  ami           = var.ec2_ami_id
+  instance_type = "t2.micro"
+  key_name      = "centos-eks"
+  count         = var.ec2_instance_count
+  #user_data = file("install.sh")  
   user_data              = <<-EOF
     #!/bin/bash
     sudo yum update -y
